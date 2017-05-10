0db81cd6d159301bba3c4a1a3154859d45a2bf51
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-05-10 01:28:23 -0300

    Creating generic app

--- 01-first-app/src/index.js
+++ 01-first-app/src/index.js
@@ -1,17 +1,7 @@
-var express = require('express');
+var runApp = require('../../lib/generic-app');
 var path = require('path');
-var open = require('open');
 
-var app = express();
+var indexPath = path.join(__dirname, 'index.html');
+var publicPath = __dirname;
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-// Making the JS file (and also all others) available to the app
-app.use(express.static(__dirname));
-
-app.listen(3000, function(err) {
-    console.log('Listening to port 3000');
-    open('http://localhost:3000');
-});
+runApp(indexPath, publicPath);
