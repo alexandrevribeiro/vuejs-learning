b03d48d0649926cf510f3ab38e182d84dcab0ad2
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-05-09 22:03:52 -0300

    Using data and methods

--- first-app/src/app.js
+++ first-app/src/app.js
@@ -3,12 +3,15 @@ window.onload = function () {
     new Vue({
         el: '#app',
         data: {
-            title: 'Hello World!'
+            title: 'Type something below...'
         },
         methods: {
             changeTitle: function(event) {
                 // Vue makes all data properties available through "this"
                 this.title = event.target.value;
+            },
+            sayHello: function() {
+                return 'Hello!'
             }
         }
     });
