eaf951c2219071a224248e61691abaca747542f4
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-05-11 00:59:36 -0300

    Small improvement in the task 02

--- practice-tasks/02 - Events/app.js
+++ practice-tasks/02 - Events/app.js
@@ -1,14 +1,11 @@
 new Vue({
-        el: '#exercise',
-        data: {
-            value: ''
-        },
-        methods: {
-            showAlert: function() {
-                alert('Button clicked!');
-            },
-            updateValue: function(event) {
-                this.value = event.target.value;
-            }
+    el: '#exercise',
+    data: {
+        value: ''
+    },
+    methods: {
+        showAlert: function () {
+            alert('Button clicked!');
         }
-    });
+    }
+});
