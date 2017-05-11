019347f01393bece24191edfb005278dcb5c800b
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-05-10 22:42:28 -0300

    Simple click event

---
+++ 02-events-app/src/vue-app.js
@@ -0,0 +1,14 @@
+
+window.onload = function () {
+    new Vue({
+        el: '#app',
+        data: {
+            counter: 0
+        },
+        methods: {
+            increase: function() {
+                this.counter++;
+            }
+        }
+    });
+}
