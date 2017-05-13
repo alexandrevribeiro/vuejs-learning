a337077d70565a41d2aafcc60a97aef6875184ed
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-05-13 17:04:29 -0300

    Improving sample

--- 03-computed-props-app/src/vue-app.js
+++ 03-computed-props-app/src/vue-app.js
@@ -3,19 +3,19 @@ window.onload = function () {
     new Vue({
         el: '#app',
         data: {
-            counter: 0,
+            firstCounter: 0,
             secondCounter: 0
         },
         methods: {
-            result: function() {
-                console.log('Method.');
-                return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+            firstCounterResult: function() {
+                console.log('Method called.');
+                return this.firstCounter > 5 ? 'Greater than 5' : 'Smaller than 5';
             }
         },
         computed: {
-            computedResult: function() {
-                console.log('Computed.');
-                return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+            secondCounterResult: function() {
+                console.log('Computed prop called.');
+                return this.secondCounter > 5 ? 'Greater than 5' : 'Smaller than 5';
             }
         }
     });
