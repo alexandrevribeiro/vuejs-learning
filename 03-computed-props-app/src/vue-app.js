1107daf8d9edffafbcf160f9cd48738328b1f377
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-05-13 21:19:12 -0300

    Watched properties

--- 03-computed-props-app/src/vue-app.js
+++ 03-computed-props-app/src/vue-app.js
@@ -4,7 +4,9 @@ window.onload = function () {
         el: '#app',
         data: {
             firstCounter: 0,
-            secondCounter: 0
+            secondCounter: 0,
+            watchedFirstState: '',
+            watchedSecondState: ''
         },
         methods: {
             firstCounterResult: function() {
@@ -16,6 +18,22 @@ window.onload = function () {
             secondCounterResult: function() {
                 console.log('Computed prop called.');
                 return this.secondCounter > 5 ? 'Greater than 5' : 'Smaller than 5';
+            }
+        },
+        watch: {
+            firstCounter: function() {
+                this.watchedFirstState = 'Watching "firstCounter": func called.';
+                var vueInst = this;
+                setTimeout(function(){
+                    vueInst.watchedFirstState = 'Watching "firstCounter": after 2 seconds (setTimeout).';
+                }, 2000);
+            },
+            secondCounter: function() {
+                this.watchedSecondState = 'Watching "secondCounter": func called.';
+                var vueInst = this;
+                setTimeout(function(){
+                    vueInst.watchedSecondState = 'Watching "secondCounter": after 2 seconds (setTimeout).';
+                }, 2000);
             }
         }
     });
