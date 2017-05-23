6c2dc0156cf8bcab224f6f33cad2a16a2158a13e
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-05-23 01:16:24 -0300

    Task 04 - Styling - Some improvements

--- practice-tasks/04 - Styling/app.js
+++ practice-tasks/04 - Styling/app.js
@@ -1,80 +1,62 @@
 new Vue({
   el: '#exercise',
   data: {
-    effectStarted: false,
+    effectClasses: {
+      highlight: false,
+      shrink: true
+    },
     attachHighlight: false,
     textBoxClass: 'text-box',
-    inputClass: '',
+    userClass: '',
     anotherInputClass: '',
-    attachAnotherClass: false,
-    borderColor: 'green',
-    borderWidth: 1,
-    width: 200,
+    anotherClassIsVisible: false,
+    myStyle: {
+      borderWidth: '1px',
+      borderColor: 'green',
+      width: '200px'
+    },
     progressBarWidth: 0,
   },
   methods: {
     startEffect: function () {
-      this.effectStarted = true;
-      this.attachHighlight = true;
-      
+
       var vm = this;
       setInterval(function () {
-        vm.attachHighlight = !vm.attachHighlight;
-      }, 3000);
+        vm.effectClasses.highlight = !vm.effectClasses.highlight;
+        vm.effectClasses.shrink = !vm.effectClasses.shrink;
+      }, 1000);
     },
-    startProgress: function() {
+    startProgress: function () {
 
       this.progressBarWidth = 1;
 
       var vm = this;
       var intervalId = setInterval(function () {
 
-        var currentWidth = vm.progressBarWidth + vm.progressBarWidth/2;
-        vm.progressBarWidth = currentWidth <= 92 
+        var currentWidth = vm.progressBarWidth + 10;
+        vm.progressBarWidth = currentWidth <= 92
           ? currentWidth
           : 92;
 
         if (vm.progressBarWidth == 92)
           clearInterval(intervalId);
 
-      }, 200);
+      }, 300);
     }
   },
   computed: {
-    effectClass: function () {
-      if (!this.effectStarted)
-        return;
-
+    coloredTextBoxClassObj: function () {
       return {
-        highlight: this.attachHighlight,
-        shrink: !this.attachHighlight
+        'orange-text-box': this.effectClasses.highlight,
+        'blue-text-box': this.effectClasses.shrink
       };
     },
-    coloredTextBoxClassObj: function() {
-      if (!this.effectStarted)
-        return;
-
+    anotherComputedClassObj: function () {
       return {
-        'orange-text-box': this.attachHighlight,
-        'blue-text-box': !this.attachHighlight
+        orange: this.anotherInputClass == 'orange' && this.anotherClassIsVisible == 'true',
+        blue: this.anotherInputClass == 'blue' && this.anotherClassIsVisible == 'true',
+        black: this.anotherInputClass == 'black' && this.anotherClassIsVisible == 'true'
       };
-    },
-    anotherComputedClassObj: function() {
-      return {
-        orange: this.anotherInputClass == 'orange' && this.attachAnotherClass == 'true',
-        blue: this.anotherInputClass == 'blue' && this.attachAnotherClass == 'true',
-        black: this.anotherInputClass == 'black' && this.attachAnotherClass == 'true'
-      };
-    },
-    stylesObj: function() {
-      return {
-        'border-width': this.borderWidth + 'px',
-        'border-color': this.borderColor,
-        'width': this.width + 'px'
-      };
-    },
-    progressBarStyle: function() {
-      return {};
     }
   }
 });
