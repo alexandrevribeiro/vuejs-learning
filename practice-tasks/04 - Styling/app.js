248a0d0cdfe7ef2941f9fca53e1e59fa24994744
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-05-23 00:32:34 -0300

    Task 04 - Styling - Exercise 6

--- practice-tasks/04 - Styling/app.js
+++ practice-tasks/04 - Styling/app.js
@@ -10,6 +10,7 @@ new Vue({
     borderColor: 'green',
     borderWidth: 1,
     width: 200,
+    progressBarWidth: 0,
   },
   methods: {
     startEffect: function () {
@@ -20,6 +21,23 @@ new Vue({
       setInterval(function () {
         vm.attachHighlight = !vm.attachHighlight;
       }, 3000);
+    },
+    startProgress: function() {
+
+      this.progressBarWidth = 1;
+
+      var vm = this;
+      var intervalId = setInterval(function () {
+
+        var currentWidth = vm.progressBarWidth + vm.progressBarWidth/2;
+        vm.progressBarWidth = currentWidth <= 92 
+          ? currentWidth
+          : 92;
+
+        if (vm.progressBarWidth == 92)
+          clearInterval(intervalId);
+
+      }, 200);
     }
   },
   computed: {
@@ -54,6 +72,9 @@ new Vue({
         'border-color': this.borderColor,
         'width': this.width + 'px'
       };
+    },
+    progressBarStyle: function() {
+      return {};
     }
   }
 });
