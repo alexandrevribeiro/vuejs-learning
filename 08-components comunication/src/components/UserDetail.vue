c69c2d4b457ec715566449a5e75ae2e68d42d670
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-06-10 18:52:37 -0300

    Components communication with Callback functions

--- 08-components comunication/src/components/UserDetail.vue
+++ 08-components comunication/src/components/UserDetail.vue
@@ -4,46 +4,43 @@
         <p>Many Details</p>
         <p>User name: {{ myName }}</p>
         <p>Reversed name: {{ reverseName() }}</p>
-        <button @click="resetName">Reset name</button>
+        <button @click="resetName">Reset name (child method)</button>
+        <button @click="resetCallback">Reset name (parent callback)</button>
     </div>
 </template>
 
 <script>
 
 export default {
-
-    data() {
-        return {
-            originalName: this.myName
-        }
-    },
     methods: {
         reverseName() {
             return this.myName.split('').reverse().join('');
         },
         resetName() {
-            this.myName = this.originalName;
+            this.myName = 'Alexandre';
             // Emittes an event to the parent component
             this.$emit('nameWasReset', this.myName);
         }
     },
 
-    // Possibilities to specify props:
-    // 1) Just the name:
-    // props: ['myName'],
+    // Possibilities to specify props:    
 
-    // 2) Validating it by type:
-    // props: {
-    //     myName: String
-    // },
-
-    // 3) With more complex validations:
+    // 1) A more complex validations:
     props: {
         myName: {
             type: String,
             required: true
-        }
+        },
+        resetCallback: Function
     }
+
+    // 2) Just the name:
+    // props: ['myName'],
+
+    // 3) Validating it by type:
+    // props: {
+    //     myName: String
+    // },
 
     // 4) With default values:
     // props: {
