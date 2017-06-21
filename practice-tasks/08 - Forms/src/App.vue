08925539e1af6871f8cd7fd236b44e093ac9f8fe
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-06-20 23:08:05 -0300

    Task 08 - Forms - Exercise 2

--- practice-tasks/08 - Forms/src/App.vue
+++ practice-tasks/08 - Forms/src/App.vue
@@ -71,9 +71,19 @@
                     <!-- which still holds the First Name and Last Name Input Field -->
                 </div>
             </div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                            class="btn btn-primary"
+                            @click.prevent="submitted">
+                            Submit!
+                    </button>
+                </div>
+            </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -83,7 +93,7 @@
                         <p>Full Name: {{ user.firstName }} {{ user.lastName }} </p>
                         <p>Mail: {{ user.email }} </p>
                         <p>Password: {{ user.password }}</p>
-                        <p>Store in Database?: {{ storesData ? 'Yes' : 'No' }} </p>
+                        <p>Store in Database? {{ storesData }} </p>
                     </div>
                 </div>
             </div>
@@ -101,8 +111,14 @@
                     email: '',
                     password: ''
                 },
-                storesData: false
+                storesData: false,
+                isSubmitted: false
             };
+        },
+        methods: {
+            submitted() {
+                this.isSubmitted = true;
+            }
         }
     }
 </script>
