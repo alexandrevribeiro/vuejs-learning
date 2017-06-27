37b002851f2f1adb11bfb0b72d917acbb3e0174e
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-06-27 20:13:07 -0300

    Vuex - using getters

--- 16-vuex/src/store/store.js
+++ 16-vuex/src/store/store.js
@@ -6,5 +6,10 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
     state: {
         count: 0
+    },
+    getters: {
+        doubleCounter: state => {
+            return state.count * 2;
+        }
     }
 });
