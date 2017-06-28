5cdafd49a670a3fa9298d17a4ae25c65d54d074f
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-06-27 23:32:40 -0300

    Vuex - Using "mapMutations"

--- 16-vuex/src/components/AnotherCounter.vue
+++ 16-vuex/src/components/AnotherCounter.vue
@@ -6,7 +6,11 @@
 </template>
 
 <script>
+    import { mapMutations } from 'vuex';
+
     export default {
+        // Option 1) Create one method for each mutation, calling the "commit".
+        /*
         methods: {
             increment() {
                 // this.$store.state.count++;
@@ -16,6 +20,18 @@
                 // this.$store.state.count--;
                 this.$store.commit('decrement');
             }
+        }
+        */
+
+        // Option 2)  Use the "mapMutations" help function to automatically
+        // map all mutations to methods. It can be an array of
+        // strings (with the getter names), or an object where you can change the name].
+        // Optionally we can use EC6 Spread operators to allow creating more methods
+        methods: {
+            ...mapMutations([
+                'increment',
+                'decrement'
+            ])
         }
     }
 </script>
