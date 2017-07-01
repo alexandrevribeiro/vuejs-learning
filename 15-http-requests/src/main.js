c37923af772082431dac46d6add35943df29008f
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-07-01 20:17:15 -0300

    Vue-resource - Intercepting Requests and Responses

--- 15-http-requests/src/main.js
+++ 15-http-requests/src/main.js
@@ -7,6 +7,17 @@ Vue.use(VueResource);
 // Configuring the root URL globally
 Vue.http.options.root = 'https://vuejs-http-ed616.firebaseio.com/data.json';
 
+// Intercepting Requests and Responses
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  // next();
+
+  // Intercepting also the response
+  next(response => {
+    console.log(response);
+  });
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
