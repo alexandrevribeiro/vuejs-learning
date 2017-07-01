e4b7b1918ddd1fba9c544f649600d10d0817ca57
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-07-01 20:06:50 -0300

    Vue-resource - Configuring the root URL globally

--- 15-http-requests/src/main.js
+++ 15-http-requests/src/main.js
@@ -4,6 +4,9 @@ import App from './App.vue'
 
 Vue.use(VueResource);
 
+// Configuring the root URL globally
+Vue.http.options.root = 'https://vuejs-http-ed616.firebaseio.com/data.json';
+
 new Vue({
   el: '#app',
   render: h => h(App)
