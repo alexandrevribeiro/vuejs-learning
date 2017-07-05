ba221366d25a8b94eda30f987cffbe3bbe9dd2e6
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-07-04 23:25:28 -0300

    Vue-router - Using "history" model

--- 16-routing/src/main.js
+++ 16-routing/src/main.js
@@ -3,11 +3,14 @@ import VueRouter from 'vue-router';
 import App from './App.vue';
 import { routes } from './routes';
 
-// Adds vue-router plugin
+// Use the vue-router middleware
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes
+  routes,
+  // The "history" mode requires the server to always return the "index.html" page.
+  // For more info see: https://router.vuejs.org/en/essentials/history-mode.html
+  mode: 'history'
 });
 
 new Vue({
