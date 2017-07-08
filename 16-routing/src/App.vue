6881660bf1748ad99a28138754a97396d17979b3
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-07-07 23:39:25 -0300

    Vue-router - Using transitions

--- 16-routing/src/App.vue
+++ 16-routing/src/App.vue
@@ -6,7 +6,9 @@
                 <hr>
                 <!--<app-header></app-header>-->
                 <router-view name="header-top"></router-view>
-                <router-view></router-view>
+                <transition name="fade" mode="out-in">
+                    <router-view></router-view>
+                </transition>
             </div>
         </div>
     </div>
@@ -23,4 +25,14 @@
 </script>
 
 <style>
+    .fade-enter {
+        opacity: 0
+    }
+    .fade-enter-active {
+        transition: opacity 1s;
+    }
+    .fade-leave-active {
+        transition: opacity 1s;
+        opacity: 0;
+    }
 </style>
