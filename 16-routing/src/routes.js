44340b46403f008800f693be1962772c202b3946
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-07-07 23:16:33 -0300

    Vue-router - Using named router views

--- 16-routing/src/routes.js
+++ 16-routing/src/routes.js
@@ -1,11 +1,17 @@
 import Home from './components/Home.vue';
+import Header from './components/Header.vue';
 import User from './components/user/User.vue';
 import UserStart from './components/user/UserStart.vue';
 import UserEdit from './components/user/UserEdit.vue';
 import UserDetail from './components/user/UserDetail.vue';
 
 export const routes = [
-    { path: '/', component: Home, name: 'home' },
+
+    { path: '/', name: 'home', /*component: Home,*/ components: {
+        default: Home,
+        'header-top': Header
+    } },
+
     // { path: '/user/:id', component: User, props: true }
     { path: '/users', component: User, children: [
         { path: '', component: UserStart },
